1f1455c93124a614cc4ee60efab4c6af308d06b5
Md Jewele Islam <mdjeweleislam@gmail.com>
2024-08-01 16:59:36 +0600

    Updated utils.py

--- sheets/utils.py
+++ sheets/utils.py
@@ -14,8 +14,6 @@ client = gspread.authorize(creds)
 # Open the sheet by its ID
 sheet = client.open_by_key("1eYejJ1ywhqlTF1KIGGmTTAzpwD6TuyvBPSqdsfHHCsA").sheet1
 
-data = ["full_name", "email", "idea_title", "industry", "description", "impact", "usp"]
-
 # data = sheet.get_all_records()
 def  append_row(data):
     sheet.append_row(data)
