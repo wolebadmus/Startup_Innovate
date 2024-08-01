7fde718e689e774e40410da4c64ab9664d8c4063
Md Jewele Islam <mdjeweleislam@gmail.com>
2024-08-01 18:54:16 +0600

    Bug fixed

--- sheets/utils.py
+++ sheets/utils.py
@@ -3,9 +3,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 import os
 
 def append_row(data, BASE_PATH):
-    json_file = os.path.join(BASE_PATH, 'client_secret.json')
-    print("CLient Secret ", json_file)
-    creds = ServiceAccountCredentials.from_json_keyfile_name(json_file)
+    creds = ServiceAccountCredentials.from_json_keyfile_name("/home/webapp/client_secret.json")
     client = gspread.authorize(creds)
     sheet = client.open_by_key("1eYejJ1ywhqlTF1KIGGmTTAzpwD6TuyvBPSqdsfHHCsA").sheet1
     sheet.append_row(data)
