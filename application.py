a6ab55164c41cb4629065d48d1bdaf18aa14837f
DebuMandal234 <freelancerdebu365@gmail.com>
2024-08-05 22:10:53 +0530

    Update application.py

--- application.py
+++ application.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 #import sheets  # Import the sheets module
 from sheets import append_row
 import os
+import requests
 
 application = Flask(__name__)
 application.secret_key = os.getenv('FLASK_SECRET_KEY', 'default_key_for_development')  # Use a default key for development
@@ -32,7 +33,25 @@ def submit():
     
  #    Append data to Google Sheets
     try:
-        append_row(data, BASE_PATH)
+        url = "https://script.google.com/macros/s/AKfycbwY5lLQ5CmivUW7-n7SRuCZDvKH4IJWVhop9i1bP03tnrTshWR0PkXYEBUzCQnSfKiBYw/exec"
+
+   
+    payload = {
+        "fullName": data[0],
+        "email": data[1],
+        "ideaTitle": data[2],
+        "industry": data[3],
+        "description": data[4],
+        "impact": data[5],
+        "usp": data[6],
+        "consent": data[7]
+    }
+
+    response = requests.post(url, data=payload)
+   
+
+        
+ #       append_row(data, BASE_PATH)
  #        flash('Your idea has been submitted successfully!')
     except Exception as e:
         print(f"ERROR {e}")
